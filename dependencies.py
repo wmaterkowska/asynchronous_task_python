eb0ce51216a368520a7bb4d8911ae37d5116ff82
Weronika Materkowska <w.m.materkowska@gmail.com>
2022-09-08 01:14:42 +0200

    new version and structure, working but not asynchronously

--- dependencies.py
+++
@@ -1,8 +0,0 @@
-from db.config import async_session
-from db.dals.task_dal import TaskDAL
-
-
-async def get_task_dal():
-    async with async_session() as session:
-        async with session.begin():
-            yield TaskDAL(session)
